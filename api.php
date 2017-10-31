ff86b7f45b797ee55ed3ea7fa2c6134a2d90a136
adash333 <adash333@hotmail.com>
2017-11-01 06:00:20 +0900

    Update api.php

--- api.php
+++ api.php
@@ -1,4 +1,12 @@
-<!-- original code from https://www.sejuku.net/blog/26754 and others -->
+<!-- https://www.sejuku.net/blog/26754 -->
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>submit</title>
+</head>
+<body >
 
 <?php
 
@@ -22,13 +30,25 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $header); // リクエストにヘッダーを含める
 curl_setopt($ch, CURLOPT_SAFE_UPLOAD, true);
 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
 //URLの情報を取得する
 $res =  curl_exec($ch);
 
 //結果を表示する
-var_dump($res);
+# var_dump(json_decode($res, true));
+$array = json_decode($res, true);
+print_r($array);
+
+echo "<br />結果：";
+echo $array["result"];
+echo "<br />確率：";
+echo $array["probability"];
 
 //セッションを終了する
 curl_close($ch);
 
 ?>
+
+</body>
+</html>
